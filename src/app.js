06211b6ae21a29be8ef6c6d6e3446ecd26bceb7d
ToninoJoya <antoniocaprab@gmail.com>
2025-03-28 18:36:14 +0000

    Excusas completo

--- src/app.js
+++ src/app.js
@@ -1,14 +1,23 @@
+import "bootstrap";
+import "./style.css";
+
+
+import "./assets/img/rigo-baby.jpg";
+import "./assets/img/4geeks.ico";
 
 window.onload = function() {
+
   let who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
   let action = ['ate', 'peed', 'crushed', 'broke'];
   let what = ['my homework', 'my phone', 'the car'];
   let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
 
-  let rdm1 = Math.floor(Math.random() * who.length);
-  let rdm2 = Math.floor(Math.random() * what.length);
-  let rdm3 = Math.floor(Math.random() * action.length);
-  let rdm4 = Math.floor(Math.random() * when.length);
+  let ramdom1 = Math.floor(Math.random() * who.length);
+  let ramdom2 = Math.floor(Math.random() * what.length);
+  let ramdom3 = Math.floor(Math.random() * action.length);
+  let ramdom4 = Math.floor(Math.random() * when.length);
 
-  document.querySelector("#excuse").innerHTML =  `${who[rdm1]}  ${action[rdm2]}  ${what[rdm3]}  ${when[rdm4]}`;
+  let excuse = document.getElementById("excuse")
+  excuse.innerHTML = `${who[ramdom1]}  ${action[ramdom2]}  ${what[ramdom3]}  ${when[ramdom4]}`;
+  
 }
